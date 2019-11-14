fedf7d6069527dc2c676b04f778b7397d4f1d0a1
Adam Powell <adampowell@adams-imac.home>
2019-11-14 11:05:51 +0000

    Updates

---
+++ precache-manifest.9cb02b3faf37f100ca303ff34a97a780.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92c1a22991c2a970812fc13450b9729",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "bd75523dd403552dd0f7",
+    "url": "/monsters-rolodex/static/css/main.7ad981db.chunk.css"
+  },
+  {
+    "revision": "3d61a31d3b4790863f30",
+    "url": "/monsters-rolodex/static/js/2.48f06037.chunk.js"
+  },
+  {
+    "revision": "bd75523dd403552dd0f7",
+    "url": "/monsters-rolodex/static/js/main.5a286967.chunk.js"
+  },
+  {
+    "revision": "3d30865087a4d44a0b65",
+    "url": "/monsters-rolodex/static/js/runtime-main.6a02ae69.js"
+  }
+]);
